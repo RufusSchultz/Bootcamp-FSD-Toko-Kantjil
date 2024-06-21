b3c1ea6b9bfb13930b0ce157b7224578fa2c25dc
RufusSchultz <rufusschultz@gmail.com>
2024-06-21 13:31:40 +0200

    initial setup

---
+++ src/main/java/com/backend/tokokantjil/TokoKantjilApplication.java
@@ -0,0 +1,13 @@
+package com.backend.tokokantjil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoKantjilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokoKantjilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/tokokantjil/TokoKantjilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.tokokantjil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoKantjilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
