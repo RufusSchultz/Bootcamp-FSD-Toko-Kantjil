f51cee35e22873ad3b534ef2512bb6c6d5addd01
RufusSchultz <rufusschultz@gmail.com>
2024-09-04 15:29:44 +0200

    Removed faulty validation constraints.

--- src/main/java/com/backend/tokokantjil/dtos/inputs/CustomerInputDto.java
+++ src/main/java/com/backend/tokokantjil/dtos/inputs/CustomerInputDto.java
@@ -1,8 +1,6 @@
 package com.backend.tokokantjil.dtos.inputs;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 
 public class CustomerInputDto {
     @NotBlank
@@ -12,9 +10,7 @@ public class CustomerInputDto {
     @NotBlank
     @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}", flags = Pattern.Flag.CASE_INSENSITIVE)
     public String email;
-    @NotBlank
-    @Pattern(regexp = "^\\d{10}$", message = "Number must be a 10 digit long number.")
+    @Positive
     public Long phoneNumber;
     public String notes;
-
 }
